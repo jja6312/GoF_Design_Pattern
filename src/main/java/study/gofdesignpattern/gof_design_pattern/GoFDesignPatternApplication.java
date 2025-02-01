1d42a1b9c845af1af1889d7efb1385aa1af567e7
정지안 <127708710+jja6312@users.noreply.github.com>
2025-02-01 21:52:20 +0900

    initial project setting

---
+++ src/main/java/study/gofdesignpattern/gof_design_pattern/GoFDesignPatternApplication.java
@@ -0,0 +1,13 @@
+package study.gofdesignpattern.gof_design_pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoFDesignPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoFDesignPatternApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/gofdesignpattern/gof_design_pattern/GoFDesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package study.gofdesignpattern.gof_design_pattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoFDesignPatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
